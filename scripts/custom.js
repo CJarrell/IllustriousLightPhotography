06300a653b6023bfb40fda00007d5d108975714b
CJarrell <colby.jarrell@icloud.com>
2017-10-02 23:52:14 -0400

    Put 2gether the bones, the nav and the jumbo

---
+++ scripts/custom.js
@@ -0,0 +1,39 @@
+// Custom JS for CJPhoto.com Site
+// Init Isotope for Jumbotron Grid
+// $(document).ready(function(){
+//   $grid = $(.grid).imagesLoaded( function(){
+//     $grid.isotope({
+//       // Options
+
+//     })
+//   })
+// })
+// Venobox Init
+$(function(){
+  $('.venobox').venobox({
+    framewidth: '1920px',        // default: ''
+    border: '3px',             // default: '0'
+    bgcolor: '#ffffff',         // default: '#fff'
+    numeratio: true,            // default: false
+    infinigall: true            // default: false
+  });
+});
+// Owl carousel
+$(function(){
+  $('#jumbo-carousel').owlCarousel({
+    items: '1.5',
+    center: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4500,
+    animateIn: true,
+    animateOut: true,
+    loop: true
+  });
+});
+// Disable Right Clicking
+$(function(){
+  $('img').on("contextmenu", function(e){
+    return false;
+  });
+});
